30191aec2cd95b68c479f7efb0e16f284f29546a
deterjan <d.ulusel@gmail.com>
2017-04-29 15:28:34 +0300

    change rest address

--- RestServer.py
+++ RestServer.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 # example request:
 # curl -i -H "Content-Type: application/json" -X POST -d '{"URLs": ["https://www.araba.com", "https://www.arabam.com", "http://www.oyunskor.com/araba-oyunlari", "https://www.sahibinden.com/kategori/otomobil", "http://www.oyunkolu.com/araba-oyunlari/"]}' http://localhost:5000/document-analyzer/api/get_tfidf_scores
-@app.route('/document-analyzer/api/get_tfidf_scores', methods=['POST'])
+@app.route('/document_analyzer/get_tfidf_scores', methods=['POST'])
 def search_wordenp():
     if not request.json or not 'URLs' in request.json:
         abort(400)
